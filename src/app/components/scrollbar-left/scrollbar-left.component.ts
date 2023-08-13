4a70fa689b68a241302dac87837154996b9f7e1e
stecala <ste.cala123@gmail.com>
2023-08-13 11:07:48 +0200

    Created component and homepage

---
+++ src/app/components/scrollbar-left/scrollbar-left.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scrollbar-left',
+  templateUrl: './scrollbar-left.component.html',
+  styleUrls: ['./scrollbar-left.component.scss']
+})
+export class ScrollbarLeftComponent {
+
+}
